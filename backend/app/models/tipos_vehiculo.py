3377255e9f97543a535a6d2c4e7558f2a8856042
MaylonRicaldi <72699993@continental.edu.pe>
2026-09-04 15:58:00 -0500

    actualizacion de documento segunda semana

--- backend/app/models/tipos_vehiculo.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text
-from .base import Base
-
-class TipoVehiculoCat(Base):
-    __tablename__ = "tipos_vehiculo"
-    id_tipo = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String(50), unique=True, nullable=False)  # camioneta, furgón, moto
-    descripcion = Column(Text, nullable=True)
